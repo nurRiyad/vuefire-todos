2abbe315cf665b9860d11620e4bc9d4a5cbc8d90
nurRiyad <asadnurriyad@gmail.com>
2023-01-29 00:56:55 +0600

    update hero test
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,11 +12,10 @@ const onClick = () => {
   <div class="hero min-h-[70vh] bg-base-100">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Procrasti-Nation</h1>
+        <h1 class="text-5xl font-bold">The Never-Ending To-Do List</h1>
         <p class="py-6">
-          Procrasti-Nation: Vue3, Firebase, VueFire, Tailwindcss, DaisyUi
-          powered to-do app. Create, update, delete tasks with ease, and user
-          authentication. Boss up your task game now!
+          Vue3, Firebase, Tailwindcss powered to-do app. Create, update, delete
+          tasks with ease, and user authentication. Boss up your task game now!
         </p>
         <button @click="onClick" class="btn btn-primary">Get Started</button>
       </div>
