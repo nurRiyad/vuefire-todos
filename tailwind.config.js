beb8acb12a4c8f024e7b00f2c7e0db4f79b1d9ea
nurRiyad <asadnurriyad@gmail.com>
2023-08-05 21:45:33 +0600

    Always use light theme
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: ["light"],
+  },
+
   plugins: [require('daisyui')],
 }
